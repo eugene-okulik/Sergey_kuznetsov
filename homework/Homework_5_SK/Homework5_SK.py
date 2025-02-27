b3868debb3a7deb5be0350723ddec067e3dc8d70
Sergey Kuznetsov <kavss@mail.ru>
2025-02-27 10:30:06 +0300

    Change homework 5

--- homework/Homework_5_SK/Homework5_SK.py
+++ homework/Homework_5_SK/Homework5_SK.py
@@ -12,9 +12,9 @@ result_operation2 = 'результат операции: 514'
 result_program = 'результат работы программы: 9'
 
 # n1 = int(result_operation[result_operation.index(':') + 2:]) + 10
-n1 = int(result_operation[result_operation.index('4'):]) + 10
-n2 = int(result_operation2[result_operation2.index('5'):]) + 10
-n3 = int(result_program[result_program.index('9'):]) + 10
+n1 = int(result_operation[result_operation.index(':') + 2:]) + 10
+n2 = int(result_operation2[result_operation2.index(':') + 2:]) + 10
+n3 = int(result_program[result_program.index(':') + 2:]) + 10
 
 print(n1, n2, n3)
 
