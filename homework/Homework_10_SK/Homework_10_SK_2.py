027731b608c7feadc7cb28374f2e76896c8dfbeb
Sergey Kuznetsov <kavss@mail.ru>
2025-03-29 12:38:59 +0300

    Homework_SergeyK_10_edit

--- homework/Homework_10_SK/Homework_10_SK_2.py
+++ homework/Homework_10_SK/Homework_10_SK_2.py
@@ -2,11 +2,13 @@ def repeat_me(fun):
     def wrapper(text, count=2):
         for i in range(count):
             fun(text)
+
     return wrapper
 
 
 @repeat_me
 def example(text):
     print(text)
+
 
 example('print me', count=2)
